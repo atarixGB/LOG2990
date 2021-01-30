dc9d8d359e54d98e2803d88bb9fcf0cfcb410dd1
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-01-30 14:16:15 -0500

    Some code refactoring, add some tools icons in sidebar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
@@ -10,10 +11,11 @@ import { EditorComponent } from './components/editor/editor.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { FileMenuComponent } from './components/sidebar/file-menu/file-menu.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ToolsListComponent } from './components/sidebar/tools-list/tools-list.component';
 
 @NgModule({
-    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent, FileMenuComponent],
-    imports: [BrowserModule, HttpClientModule, AppRoutingModule, MatIconModule, MatButtonModule],
+    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent, FileMenuComponent, ToolsListComponent],
+    imports: [BrowserModule, HttpClientModule, AppRoutingModule, MatIconModule, MatButtonModule, MatListModule],
     providers: [],
     bootstrap: [AppComponent],
 })
