c998f80b73a0b1ed9a3afd4f239c50d22ecc17a7
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-01-30 13:08:49 -0500

    Create component for the file menu and adjust buttons

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,11 +8,12 @@ import { AppComponent } from './components/app/app.component';
 import { DrawingComponent } from './components/drawing/drawing.component';
 import { EditorComponent } from './components/editor/editor.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { FileMenuComponent } from './components/sidebar/file-menu/file-menu.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent],
-    imports: [BrowserModule, HttpClientModule, AppRoutingModule, MatIconModule],
+    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent, FileMenuComponent],
+    imports: [BrowserModule, HttpClientModule, AppRoutingModule, MatIconModule, MatButtonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
