aa76f5c7554bfaddc4afd0253ede420a8d45e40b
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-02-05 23:47:45 -0500

    Finally got my coordinates

--- client/src/app/services/tools/line.service.ts
+++ client/src/app/services/tools/line.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+// import { Segment } from '@app/classes/segment';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DEFAULT_JUNCTION_RADIUS, DEFAULT_LINE_THICKNESS, MouseButton, TypeOfJunctions } from '@app/constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+
+const WHITE: string = '#ffffff';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LineService extends Tool {
     private pathData: Vec2[];
+    private firstClickedCoord: Vec2;
+    private lastCoordinate: Vec2;
+    private coords: Vec2[];
     lineWidth: number;
     junctionType: TypeOfJunctions;
     junctionRadius: number;
@@ -18,11 +24,17 @@ export class LineService extends Tool {
         this.lineWidth = DEFAULT_LINE_THICKNESS;
         this.junctionRadius = DEFAULT_JUNCTION_RADIUS;
         this.junctionType = TypeOfJunctions.REGULAR;
+        this.coords = [];
     }
 
     onMouseClick(event: MouseEvent): void {
         this.mouseDown = event.button === MouseButton.Left;
         this.mouseDownCoord = this.getPositionFromMouse(event);
+
+        if (this.firstClickedCoord === undefined) {
+            this.firstClickedCoord = this.mouseDownCoord;
+            console.log('first click:' + this.firstClickedCoord.x, this.firstClickedCoord.y);
+        }
 
         if (this.junctionType === TypeOfJunctions.CIRCLE) {
             this.drawingService.baseCtx.lineWidth = this.lineWidth;
@@ -44,6 +56,10 @@ export class LineService extends Tool {
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
             const mousePosition = this.getPositionFromMouse(event);
+            this.lastCoordinate = mousePosition;
+            this.coords.push(this.lastCoordinate);
+            console.log('coords: ' + this.coords[this.coords.length - 1].x + this.coords[this.coords.length - 1].y);
+            console.log('last coordinates: ' + this.lastCoordinate.x, this.lastCoordinate.y);
             this.pathData.push(mousePosition);
             this.drawLine(this.drawingService.baseCtx, this.pathData);
         }
@@ -72,7 +88,7 @@ export class LineService extends Tool {
                 // TODO
                 break;
             case 'Backspace':
-                // TODO
+                this.removeLastSegment(this.drawingService.baseCtx, this.coords);
                 break;
             default:
                 break;
@@ -86,6 +102,20 @@ export class LineService extends Tool {
         ctx.lineTo(path[path.length - 1].x, path[path.length - 1].y); // Get last point of pathData
         ctx.stroke(); // Stroke a line between these two points
     }
+
+    private removeLastSegment(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
+        ctx.lineWidth = this.lineWidth + 10;
+        ctx.strokeStyle = WHITE;
+        ctx.beginPath();
+        ctx.moveTo(path[path.length - 2].x, path[path.length - 2].y); // Get first point of pathData
+        ctx.lineTo(path[path.length - 1].x, path[path.length - 1].y); // Get last point of pathData
+        ctx.stroke(); // Stroke a line between these two points
+    }
+
+    // private isCloseToXAxis(coords: Vec2): boolean {
+    //   let angle = Math.tan(coords.y/coords.x);
+    //   return angle >= 0 && angle < 45;
+    // }
 
     private clearPath(): void {
         this.pathData = [];
