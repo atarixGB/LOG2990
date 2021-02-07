756b2a72bd6c9697d86ff6ee0581b906581a4a9a
miora.rr <miora.r.rakoto@gmail.com>
2021-02-07 15:48:19 -0500

    hotkeys sont dans tool manager

--- client/src/app/services/tools/tool-manager.service.ts
+++ client/src/app/services/tools/tool-manager.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
 import { ToolList } from '@app/constants';
 import { EllipseService } from './ellipse/ellipse.service';
 import { LineService } from './line/line.service';
@@ -11,11 +12,81 @@ import { PencilService } from './pencil/pencil-service';
 export class ToolManagerService {
     private currentTool: Tool;
     private currentToolEnum: ToolList;
+    mousePosition: Vec2;
     toolList: ToolList;
 
     constructor(private pencilService: PencilService, private lineService: LineService, private ellipseService: EllipseService) {
         this.currentTool = this.pencilService;
         this.currentToolEnum = ToolList.Pencil;
+    }
+
+    handleHotKeysShortcut(event: KeyboardEvent): void {
+        switch (event.key) {
+            case 'c':
+                this.currentTool = this.pencilService;
+                this.currentToolEnum = ToolList.Pencil;
+                break;
+
+            case 'a':
+                // TODO aerosol
+                break;
+
+            case '1':
+                // this.currentTool = this.rectangleService;
+                // this.currentToolEnum = ToolList.Rectangle;
+                break;
+
+            case '2':
+                this.currentTool = this.ellipseService;
+                this.currentToolEnum = ToolList.Ellipse;
+                break;
+
+            case '3':
+                // TODO Polygone
+                break;
+
+            case 'l':
+                this.currentTool = this.lineService;
+                this.currentToolEnum = ToolList.Line;
+                break;
+
+            case 't':
+                // TODO outil texte
+                break;
+
+            case 'b':
+                // TODO sceau de peinture
+                break;
+
+            case 'e':
+                // this.currentTool = this.eraserService;
+                // this.currentToolEnum = ToolList.Eraser;
+                break;
+            case 'd':
+                // TODO etampe
+                break;
+
+            case 'i':
+                // TODO pipette
+                break;
+
+            // case 'r':
+            //     // TODO rectangle de selection
+            //     break;
+            // case 's':
+            //     // TODO ellipse de selection
+            //     break;
+            case 'v':
+                // TODO lasso polygonal
+                break;
+
+            case 'Shift':
+                if (this.currentTool === this.ellipseService) {
+                    this.currentTool.mouseCoord = this.mousePosition;
+                    this.currentTool.handleKeyDown(event);
+                }
+                break;
+        }
     }
 
     getCurrentTool(): Tool {
