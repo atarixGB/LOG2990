c6c7305e14ee3ed1fb4b6c9b77383202f8e9ec07
Nassim boughedaoui <mohamed-nassim.boughedaouI@polymtl.ca>
2021-02-07 14:31:12 -0500

    rectangle outil

---
+++ client/src/app/services/rectangle.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+
+// TODO : Déplacer ça dans un fichier séparé accessible par tous
+export enum MouseButton {
+    Left = 0,
+    Middle = 1,
+    Right = 2,
+    Back = 3,
+    Forward = 4,
+}
+@Injectable({
+    providedIn: 'root',
+})
+export class RectangleService extends Tool {
+    private pathData: Vec2[];
+    constructor(drawingService: DrawingService) {
+        super(drawingService);
+        this.clearPath();
+    }
+    private clearPath(): void {
+        this.pathData = [];
+    }
+    onMouseUp(event: MouseEvent): void {
+        if (this.mouseDown) {
+            const mousePosition = this.getPositionFromMouse(event);
+            this.pathData.push(mousePosition);
+            this.drawRectangle(this.drawingService.baseCtx, this.pathData);
+        }
+        this.mouseDown = false;
+        this.clearPath();
+    }
+
+    onMouseMove(event: MouseEvent): void {
+        if (this.mouseDown) {
+            const mousePosition = this.getPositionFromMouse(event);
+            this.pathData.push(mousePosition);
+
+            // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            this.drawRectangle(this.drawingService.previewCtx, this.pathData);
+        }
+    }
+    private drawRectangle(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
+        ctx.beginPath();
+        const firstPoint = path[0];
+        const finalPoint = path[this.pathData.length - 1];
+        const width = finalPoint.y - firstPoint.y;
+        const length = finalPoint.x - firstPoint.x;
+        ctx.rect(firstPoint.x, firstPoint.y, width, length);
+        ctx.stroke();
+    }
+}
