247d02367ef482fc0155e1a310f9cbc1db23e94e
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-02-07 13:33:42 -0500

    Fix eraser to be in real-time

--- client/src/app/services/tools/eraser.service.ts
+++ client/src/app/services/tools/eraser.service.ts
@@ -63,7 +63,7 @@ export class EraserService extends Tool {
 
             // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
-            this.drawLine(this.drawingService.previewCtx, this.pathData);
+            this.drawLine(this.drawingService.baseCtx, this.pathData);
         }
     }
 
