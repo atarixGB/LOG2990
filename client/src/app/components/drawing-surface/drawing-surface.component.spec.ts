b27d2c27c31caadae955c4dda23bfd5f1efb7165
miora.rr <miora.r.rakoto@gmail.com>
2021-02-07 23:46:10 -0500

    ajout component drawing-surface

---
+++ client/src/app/components/drawing-surface/drawing-surface.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DrawingSurfaceComponent } from './drawing-surface.component';
+
+describe('DrawingSurfaceComponent', () => {
+  let component: DrawingSurfaceComponent;
+  let fixture: ComponentFixture<DrawingSurfaceComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DrawingSurfaceComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DrawingSurfaceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
