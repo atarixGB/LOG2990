a24a8b67555bfb7b3d6d3152c3eb5920025e617f
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-02-08 11:55:15 -0500

    Add line width control to rectangle tool

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { EraserConfigComponent } from './components/sidebar/eraser-config/eraser
 import { FileMenuComponent } from './components/sidebar/file-menu/file-menu.component';
 import { LineConfigComponent } from './components/sidebar/line-config/line-config.component';
 import { PencilConfigComponent } from './components/sidebar/pencil-config/pencil-config.component';
+import { RectangleConfigComponent } from './components/sidebar/rectangle-config/rectangle-config.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ToolsListComponent } from './components/sidebar/tools-list/tools-list.component';
 
@@ -38,6 +39,7 @@ import { ToolsListComponent } from './components/sidebar/tools-list/tools-list.c
         LineConfigComponent,
         DrawingSurfaceComponent,
         EllipseConfigComponent,
+        RectangleConfigComponent,
     ],
     imports: [
         BrowserModule,
