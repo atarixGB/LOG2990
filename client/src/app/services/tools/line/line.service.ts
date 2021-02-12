0e7771a2846eb2d06178d8a7b9afe24d70c2b80b
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-02-11 23:15:04 -0500

    Ajout du 45 et 135 degres au Shift event

--- client/src/app/services/tools/line/line.service.ts
+++ client/src/app/services/tools/line/line.service.ts
@@ -5,16 +5,21 @@ import { Vec2 } from '@app/classes/vec2';
 import { DEFAULT_JUNCTION_RADIUS, DEFAULT_LINE_THICKNESS, MouseButton, TypeOfJunctions } from '@app/constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
+const SECOND_LAST_INDEX = -2;
+const NEGATIVE_LINE_SLOPE = -1;
+const NUMBER_SIGN_CHANGE = -1;
+
 @Injectable({
     providedIn: 'root',
 })
 export class LineService extends Tool {
     private pathData: Vec2[];
     private coordinates: Vec2[];
+
     private hasPressedShiftKey: boolean;
     private basePoint: Vec2;
 
-    private lastCanvasImage: ImageData[];
+    private lastCanvasImages: ImageData[];
 
     lineWidth: number;
     junctionType: TypeOfJunctions;
@@ -26,7 +31,7 @@ export class LineService extends Tool {
         this.junctionRadius = DEFAULT_JUNCTION_RADIUS;
         this.junctionType = TypeOfJunctions.REGULAR;
         this.coordinates = [];
-        this.lastCanvasImage = [];
+        this.lastCanvasImages = [];
         this.hasPressedShiftKey = false;
     }
 
@@ -62,15 +67,13 @@ export class LineService extends Tool {
         }
 
         if (this.hasPressedShiftKey) {
-            this.drawConstraintLine(this.drawingService.baseCtx, this.coordinates, event);
+            this.drawConstrainedLine(this.drawingService.baseCtx, this.coordinates, event);
         }
 
         if (this.pathData != undefined) {
-            this.lastCanvasImage.push(
+            this.lastCanvasImages.push(
                 this.drawingService.baseCtx.getImageData(0, 0, this.drawingService.canvas.width, this.drawingService.canvas.height),
             );
-            console.log(this.lastCanvasImage);
-            console.log(this.lastCanvasImage.length);
         }
 
         this.mouseDown = false;
@@ -85,7 +88,7 @@ export class LineService extends Tool {
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
 
             if (this.hasPressedShiftKey) {
-                this.drawConstraintLine(this.drawingService.previewCtx, this.coordinates, event);
+                this.drawConstrainedLine(this.drawingService.previewCtx, this.coordinates, event);
             } else {
                 this.drawLine(this.drawingService.previewCtx, this.pathData);
             }
@@ -106,15 +109,66 @@ export class LineService extends Tool {
             case 'Backspace':
                 console.log(event.key);
                 this.drawingService.clearCanvas(this.drawingService.baseCtx);
-                this.drawingService.baseCtx.putImageData(this.lastCanvasImage[this.lastCanvasImage.length - 2], 0, 0);
+                // We use the second last index to get the canvas state just before last stroked line
+                this.drawingService.baseCtx.putImageData(this.lastCanvasImages[this.lastCanvasImages.length + SECOND_LAST_INDEX], 0, 0);
                 break;
         }
     }
 
-    onKeyUp(event: KeyboardEvent): void {
+    handleKeyUp(event: KeyboardEvent): void {
         if (event.key === 'Shift') {
             this.hasPressedShiftKey = false;
         }
+    }
+
+    // Equation of a line: 0 = ax + by + c
+    // Distance from a point A to a line L :  distance(A,L) =  abs(ax + by + c) / sqrt(a^2 + b^2)
+    private getDistanceBetweenPointAndLine(point: Vec2, line: number[]): number {
+        const numerator = Math.abs(line[0] * point.x + line[1] * point.y + line[2]);
+        const denominator = Math.sqrt(line[0] * line[0] + line[1] * line[1]);
+        return numerator / denominator;
+    }
+
+    private getClosestLine(currentPoint: Vec2, basePoint: Vec2): number[] {
+        const lineList = [
+            [1, 1, NUMBER_SIGN_CHANGE * (basePoint.x + basePoint.y)], // ascending diagonal
+            [1, NEGATIVE_LINE_SLOPE, NUMBER_SIGN_CHANGE * (basePoint.x - basePoint.y)], // descending diagonal
+            [1, 0, -basePoint.x], // x axis
+            [0, 1, -basePoint.y], // y axis
+        ];
+        const distance = lineList.map((line) => this.getDistanceBetweenPointAndLine(currentPoint, line));
+        const maxDistance = Math.min(...distance);
+        const maxIndex = distance.indexOf(maxDistance);
+        console.log('closest line: ', lineList[maxIndex]);
+        return lineList[maxIndex];
+    }
+
+    // Cramer's Rule is used for solving the linear system equation
+    //    ax + by = r and cx + dy = f
+    private solveLinearEquationsSystem(a: number, b: number, c: number, d: number, e: number, f: number): Vec2 {
+        const determinant = a * d - b * c;
+        const point: Vec2 = {
+            x: (d * e - b * f) / determinant,
+            y: (a * f - c * e) / determinant,
+        };
+        return point;
+    }
+
+    private getProjectionOnClosestLine(point: Vec2, line: number[]): Vec2 {
+        const projection = this.solveLinearEquationsSystem(line[0], line[1], -line[1], line[0], -line[2], line[0] * point.y - line[1] * point.x);
+        return projection;
+    }
+
+    private getNearestPoint(currentPoint: Vec2, basePoint: Vec2): Vec2 {
+        const nearestLine: number[] = this.getClosestLine(currentPoint, basePoint);
+        return this.getProjectionOnClosestLine(currentPoint, nearestLine);
+    }
+
+    private calculatePosition(currentPoint: Vec2, basePoint: Vec2): Vec2 | undefined {
+        if (!currentPoint || !basePoint) {
+            return undefined;
+        }
+        return this.getNearestPoint(currentPoint, basePoint);
     }
 
     private drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
@@ -125,28 +179,16 @@ export class LineService extends Tool {
         ctx.stroke();
     }
 
-    private isCloseToXAxis(currentPoint: Vec2, basePoint: Vec2): boolean {
-        let projectionOnXAxis = Math.abs(currentPoint.x - basePoint.x);
-        let projectionOnYAxis = Math.abs(currentPoint.y - basePoint.y);
-        return projectionOnXAxis > projectionOnYAxis;
-    }
-
-    private drawConstraintLine(ctx: CanvasRenderingContext2D, path: Vec2[], event: MouseEvent): void {
+    private drawConstrainedLine(ctx: CanvasRenderingContext2D, path: Vec2[], event: MouseEvent): void {
         const mousePosition = this.getPositionFromMouse(event);
         this.basePoint = path[path.length - 1];
 
+        const point: Vec2 | undefined = this.calculatePosition(mousePosition, this.basePoint);
         ctx.lineWidth = this.lineWidth;
         ctx.beginPath();
-
-        if (this.isCloseToXAxis(mousePosition, this.basePoint)) {
-            console.log('horizontal');
+        if (point) {
             ctx.moveTo(this.basePoint.x, this.basePoint.y);
-            ctx.lineTo(mousePosition.x, this.basePoint.y);
-            ctx.stroke();
-        } else {
-            console.log('vertical');
-            ctx.moveTo(this.basePoint.x, this.basePoint.y);
-            ctx.lineTo(this.basePoint.x, mousePosition.y);
+            ctx.lineTo(point.x, point.y);
             ctx.stroke();
         }
     }
