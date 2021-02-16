3b969475d4951d8b37b72ebca9fc099ec397aed6
unknown <audrey-1.leblanc@polymtl.ca>
2021-02-16 18:34:29 -0500

    config-panel

--- client/src/app/components/config-panel/config-panel.component.spec.ts
+++ client/src/app/components/config-panel/config-panel.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ConfigPanelComponent } from './config-panel.component';
 
 describe('ConfigPanelComponent', () => {
@@ -20,5 +19,12 @@ describe('ConfigPanelComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should toggle config panel', () => {
+        component.isOpened = false;
+
+        component.toggleConfigPanel();
+        expect(component.isOpened).toEqual(true);
     });
 });
