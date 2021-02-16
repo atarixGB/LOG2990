9dccf93385d372476d919cbd3708b857777e776c
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-02-16 14:37:15 -0500

    Ajout de tests dans rectangle

--- client/src/app/services/tools/rectangle/rectangle.service.spec.ts
+++ client/src/app/services/tools/rectangle/rectangle.service.spec.ts
@@ -1,15 +1,58 @@
 import { TestBed } from '@angular/core/testing';
+import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
+import { Vec2 } from '@app/classes/vec2';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 import { RectangleService } from './rectangle.service';
 
-describe('RectangleService', () => {
+// tslint:disable
+fdescribe('RectangleService', () => {
     let service: RectangleService;
+    let canvasTestHelper: CanvasTestHelper;
+    let drawServiceSpy: jasmine.SpyObj<DrawingService>;
+
+    let baseCtxStub: CanvasRenderingContext2D;
+    let previewCtxStub: CanvasRenderingContext2D;
+    let drawRectangleSpy: jasmine.Spy<any>;
+    let drawSquareSpy: jasmine.Spy<any>;
+
+    let mockPathData: Vec2[];
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        drawServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
+
+        TestBed.configureTestingModule({
+            providers: [{ provide: DrawingService, useValue: drawServiceSpy }],
+        });
+        canvasTestHelper = TestBed.inject(CanvasTestHelper);
+        baseCtxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+        previewCtxStub = canvasTestHelper.drawCanvas.getContext('2d') as CanvasRenderingContext2D;
+
         service = TestBed.inject(RectangleService);
+        service['drawingService'].baseCtx = baseCtxStub; // Jasmine doesnt copy properties with underlying data
+        service['drawingService'].previewCtx = previewCtxStub;
+        service['drawingService'].canvas = canvasTestHelper.canvas;
+
+        mockPathData = [
+            { x: 10, y: 10 },
+            { x: 100, y: 100 },
+        ];
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('drawShape should call drawRectangle is isShiftShape is false', () => {
+        service['isShiftShape'] = false;
+        drawRectangleSpy = spyOn<any>(service, 'drawRectangle').and.stub();
+        service.drawShape(previewCtxStub, mockPathData);
+        expect(drawRectangleSpy).toHaveBeenCalled();
+    });
+
+    it('drawShape should call drawSquare is isShiftShape is true', () => {
+        service['isShiftShape'] = true;
+        drawSquareSpy = spyOn<any>(service, 'drawSquare').and.stub();
+        service.drawShape(previewCtxStub, mockPathData);
+        expect(drawSquareSpy).toHaveBeenCalled();
     });
 });
