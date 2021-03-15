9ae8f963859e59748d1133a96af3ad3159fe6099
miora.rr <miora.r.rakoto@gmail.com>
2021-03-15 11:39:29 -0400

    touches du clavier font defiler images

--- client/src/app/components/carousel/carouel-drawings/carousel-drawing/carousel-drawing.component.ts
+++ client/src/app/components/carousel/carouel-drawings/carousel-drawing/carousel-drawing.component.ts
@@ -1,18 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-carousel-drawing',
     templateUrl: './carousel-drawing.component.html',
     styleUrls: ['./carousel-drawing.component.scss'],
 })
-export class CarouselDrawingComponent implements OnInit {
+export class CarouselDrawingComponent {
     @Input() imagesURL: string;
     name: string;
     tags: string[];
     constructor() {
-        this.imagesURL = 'https://image.freepik.com/vecteurs-libre/animal-dessin-style-boho-icone-vector-illustration-graphique_25030-12802.jpg';
         this.name = 'Mon dessin';
         this.tags = ['test', 'dessin'];
     }
-    ngOnInit(): void {}
 }
--- client/src/app/components/carousel/carousel-modal/carousel.component.ts
+++ client/src/app/components/carousel/carousel-modal/carousel.component.ts
@@ -1,12 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-carousel',
     templateUrl: './carousel.component.html',
     styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent implements OnInit {
-    constructor() {}
+export class CarouselComponent {
+    images: string[];
+    placement: string[];
+    private index: number;
+    private afterNext: boolean;
+    private afterPrevious: boolean;
+    //  private nextN: number;
+    constructor() {
+        this.index = 0;
+        this.images = [
+            'https://image.freepik.com/vecteurs-libre/animal-dessin-style-boho-icone-vector-illustration-graphique_25030-12802.jpg',
+            'https://cdn.arstechnica.net/wp-content/uploads/2017/07/ms-paint-rip-800x500.png',
+            'https://1gew6o3qn6vx9kp3s42ge0y1-wpengine.netdna-ssl.com/wp-content/uploads/sites/140/2014/01/Boston.jpg',
+            'https://digitalsynopsis.com/wp-content/uploads/2018/03/grandma-creates-beautiful-artwork-in-ms-paint-14.jpg',
+        ];
+        this.placement = ['', '', ''];
+        this.nextImages();
+        this.afterNext = false;
+        this.afterPrevious = false;
+    }
 
-    ngOnInit(): void {}
+    nextImages() {
+        console.log('dans next');
+        if (this.afterPrevious) {
+            this.index++;
+            this.afterPrevious = false;
+        }
+        for (let i = 0; i < 3; i++) {
+            if (this.index > this.images.length - 1) {
+                this.index = 0;
+            }
+            console.log(this.index);
+            this.placement[i] = this.images[this.index];
+            this.index++;
+        }
+        this.afterNext = true;
+    }
+    previousImages() {
+        console.log('dans previous');
+        if (this.afterNext) {
+            this.afterNext = false;
+            this.index = this.index - 3;
+            console.log('dans after next ' + this.index);
+        }
+        for (let i = 2; i >= 0; i--) {
+            this.index--;
+            if (this.index < 0) {
+                this.index = this.images.length - 1;
+            }
+            console.log(this.index);
+            this.placement[i] = this.images[this.index];
+            this.afterNext = false;
+        }
+        this.afterPrevious = true;
+    }
+
+    @HostListener('document:keydown', ['$event'])
+    handleKeyDown(event: KeyboardEvent): void {
+        console.log(event.code);
+        if (event.code == 'ArrowLeft') {
+            this.previousImages();
+        }
+        if (event.code == 'ArrowRight') {
+            this.nextImages();
+        }
+    }
 }
