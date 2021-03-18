6b586b6ecc52c15c45daeba766f46398cc1919c9
Ju99 <jules99@LAPTOP-JC9MVT6T.localdomain>
2021-03-18 05:46:57 -0400

    ajout des configurations des sélections et du bouton de tout sélectionner

---
+++ client/src/app/components/sidebar/selection-config/selection-config.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { SelectionService } from '@app/services/tools/selection/selection.service';
+
+@Component({
+    selector: 'app-selection-config',
+    templateUrl: './selection-config.component.html',
+    styleUrls: ['./selection-config.component.scss'],
+})
+export class SelectionConfigComponent implements OnInit {
+    constructor(public selectionService: SelectionService) {}
+
+    ngOnInit(): void {}
+}
