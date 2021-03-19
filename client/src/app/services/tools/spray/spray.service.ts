d7fb76ab920b050534603365c0a7eef5b6a9b48f
Noursen12 <houidhek.noursen@gmail.com>
2021-03-19 14:04:31 -0400

    test pour service aerosol completé couverture à 90.91

--- client/src/app/services/tools/spray/spray.service.ts
+++ client/src/app/services/tools/spray/spray.service.ts
@@ -37,7 +37,7 @@ export class SprayService extends Tool implements OnDestroy {
     sprayFrequency: number = this.minFrequency;
     canvasData: ImageData;
 
-    constructor(drawingService: DrawingService, private colorManager: ColorManagerService) {
+    constructor(drawingService: DrawingService, public colorManager: ColorManagerService) {
         super(drawingService);
     }
 
@@ -61,7 +61,6 @@ export class SprayService extends Tool implements OnDestroy {
     onMouseUp(): void {
         if (this.mouseDown) {
             clearTimeout(this.timeoutId);
-            // this.drawingService.applyPreview();
             this.canvasData = this.drawingService.getCanvasData();
             this.updateSprayData();
         }
@@ -77,7 +76,6 @@ export class SprayService extends Tool implements OnDestroy {
     onMouseLeave(): void {
         if (this.mouseDown) {
             clearTimeout(this.timeoutId);
-            // this.drawingService.applyPreview();
         }
     }
 
