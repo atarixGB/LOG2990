4018b11c3f319abfc61805790131eb27ea947d88
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-03-23 00:50:43 -0400

    database.service à ~52% coverage

--- server/app/services/database.service.spec.ts
+++ server/app/services/database.service.spec.ts
@@ -4,11 +4,11 @@ import * as chaiAsPromised from 'chai-as-promised';
 import { describe } from 'mocha';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { DrawingData } from '../classes/drawing-data';
-import { DatabaseServiceMock } from './database.service.mock';
+import { DatabaseService } from './database.service';
 chai.use(chaiAsPromised); // this allows us to test for rejection
 
 describe('Database service', () => {
-    let databaseService: DatabaseServiceMock;
+    let databaseService: DatabaseService;
     let mongoServer: MongoMemoryServer;
     let validDrawing: DrawingData;
     // const options: MongoClientOptions = {
@@ -19,7 +19,7 @@ describe('Database service', () => {
     // let clientTest: MongoClient
 
     beforeEach(async () => {
-        databaseService = new DatabaseServiceMock();
+        databaseService = new DatabaseService();
         mongoServer = new MongoMemoryServer();
 
         validDrawing = {
@@ -38,6 +38,11 @@ describe('Database service', () => {
         }
     });
 
+    it('should instanciate correctly', (done: Mocha.Done) => {
+        chai.expect(databaseService).to.be.instanceOf(DatabaseService);
+        done();
+    });
+
     it('should connect to the database when start is called', async () => {
         const mongoUri = await mongoServer.getUri();
         await databaseService.start(mongoUri);
@@ -52,30 +57,26 @@ describe('Database service', () => {
         expect(databaseService['client'].isConnected()).to.be.false;
     });
 
-    it('should add drawing', async () => {
-        databaseService
-            .addDrawing(validDrawing)
-            .then((result) => {
-                return expect(result).to.equal({
-                    _id: '123',
-                    title: 'title',
-                    labels: ['tag1', 'tag2'],
-                    height: 0,
-                    width: 0,
-                    body: 'data:image/png;base64,imagedatahere12345',
-                });
-            })
-            .catch();
+    it('should add drawing', (done: Mocha.Done) => {
+        databaseService.addDrawing(validDrawing).then((result) => {
+            return expect(result).to.equal({
+                _id: '123',
+                title: 'title',
+                labels: ['tag1', 'tag2'],
+                height: 0,
+                width: 0,
+                body: 'data:image/png;base64,imagedatahere12345',
+            });
+        });
+        done();
     });
 
-    it('should delete drawing', async () => {
+    it('should delete drawing', (done: Mocha.Done) => {
         const drawingToRemove: string = 'title';
 
-        databaseService
-            .deleteDrawingByIdName(drawingToRemove)
-            .then((result) => {
-                return expect(result).to.equal(null);
-            })
-            .catch();
+        databaseService.deleteDrawingByIdName(drawingToRemove).then((result) => {
+            return expect(result).to.equal(null);
+        });
+        done();
     });
 });
