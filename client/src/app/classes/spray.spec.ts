406941102ba0b2540449cc5478530f1e0c0b068d
Noursen12 <houidhek.noursen@gmail.com>
2021-03-23 20:32:24 -0400

    test classe Spray pour undo redo

---
+++ client/src/app/classes/spray.spec.ts
@@ -0,0 +1,25 @@
+
+import { TestBed } from '@angular/core/testing';
+import { Spray } from '@app/classes/Spray';
+import { CanvasTestHelper } from './canvas-test-helper';
+
+
+fdescribe('Spray', () => {
+    let canvasTestHelper:CanvasTestHelper;
+    beforeEach(()=>{
+        canvasTestHelper=TestBed.inject(CanvasTestHelper);
+
+    });
+    let image:ImageData;
+    it('should create an instance', () => {
+        expect(new Spray(image)).toBeTruthy();
+    });
+
+    it('draw should update context with image data', () => {
+        const ctx = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+        const spray = new Spray(image);
+        const spy = spyOn<any>(ctx, 'putImageData');
+        spray.draw(ctx);
+        expect(spy).toHaveBeenCalled();
+    });
+});
