5544bb3373d73a8d73583de7f88a6b4527b0bad0
miora.rr <miora.r.rakoto@gmail.com>
2021-03-29 12:20:16 -0400

    backspace qui marche sans stack de ce qu'on a ecrit avant

--- client/src/app/services/tools/text/text.service.ts
+++ client/src/app/services/tools/text/text.service.ts
@@ -3,48 +3,44 @@ import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
-
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-export class TextService extends Tool{
+export class TextService extends Tool {
+    private textInput: string;
 
-    private textInput : string;
+    constructor(drawingService: DrawingService) {
+        super(drawingService);
+        this.textInput = '';
+    }
 
-  constructor(drawingService: DrawingService) { 
-    super(drawingService);
-    this.textInput = "";
-  }
+    onMouseDown(event: MouseEvent): void {
+        if (!this.mouseMove) {
+            this.mouseDownCoord = this.getPositionFromMouse(event);
+            // this.write(this.mouseDownCoord);
+        }
+        this.mouseMove = false;
+    }
 
-  onMouseClick(event : MouseEvent): void{
-      if(!this.mouseMove){
-        this.mouseDownCoord = this.getPositionFromMouse(event);
-        this.write(this.mouseDownCoord);
-      }
-      this.mouseMove = false;
-  }
+    handleKeyUp(event: KeyboardEvent): void {
+        console.log('J écris');
+        console.log(this.textInput);
+        if (event.key === 'Backspace') {
+            console.log('Problème backspace');
+            this.textInput = this.textInput.substring(0, this.textInput.length - 1);
+            console.log(this.textInput);
+        } else {
+            this.textInput += event.key;
+        }
+        this.drawingService.previewCtx.fillStyle = 'black';
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
+        this.drawingService.previewCtx.fillText(this.textInput, this.mouseDownCoord.x, this.mouseDownCoord.y);
+    }
 
-  handleKeyUp(event: KeyboardEvent): void{
-      console.log('J écris');
-      if(event.key === "BACKSPACE" && this.textInput !== ""){
-        console.log('Problème backspace');
-        this.textInput.slice(this.textInput.length-1);
-      }
-      else{
-        this.textInput += event.key;
-      }
-      this.drawingService.previewCtx.fillStyle = 'black';
-      this.drawingService.previewCtx.fillText(this.textInput, this.mouseDownCoord.x, this.mouseDownCoord.y);
-  }
-
-  write( mousePosition : Vec2) : void {
-    //this.drawingService.previewCtx.clearRect(this.mouseDownCoord.x,this.mouseDownCoord.y,this.drawingService.previewCanvas.width, this.drawingService.previewCanvas.height);
-    this.drawingService.baseCtx.fillStyle = 'black';
-    this.drawingService.baseCtx.fillText(this.textInput, mousePosition.x, mousePosition.y);
-    this.textInput = '';
-  }
-
-
-
-
+    write(mousePosition: Vec2): void {
+        //this.drawingService.previewCtx.clearRect(this.mouseDownCoord.x,this.mouseDownCoord.y,this.drawingService.previewCanvas.width, this.drawingService.previewCanvas.height);
+        this.drawingService.baseCtx.fillStyle = 'black';
+        this.drawingService.baseCtx.fillText(this.textInput, mousePosition.x, mousePosition.y);
+        this.textInput = '';
+    }
 }
