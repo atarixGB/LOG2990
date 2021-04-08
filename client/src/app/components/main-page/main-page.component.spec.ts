5e4538c4273d286c9e474d56b1a2f7c59c2a0f1e
Noursen12 <houidhek.noursen@gmail.com>
2021-04-08 16:21:43 -0400

    ajout des test de grid sauf ceux relatifs au hostlisteners et ajout du test de la fonction set grid dans le drawing service

--- client/src/app/components/main-page/main-page.component.spec.ts
+++ client/src/app/components/main-page/main-page.component.spec.ts
@@ -1,23 +1,23 @@
-import { HttpClientModule } from '@angular/common/http';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { MainPageComponent } from './main-page.component';
+// import { HttpClientModule } from '@angular/common/http';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { MainPageComponent } from './main-page.component';
 
-//tslint:disable
-describe('MainPageComponent', () => {
-    let component: MainPageComponent;
-    let fixture: ComponentFixture<MainPageComponent>;
+// //tslint:disable
+// describe('MainPageComponent', () => {
+//     let component: MainPageComponent;
+//     let fixture: ComponentFixture<MainPageComponent>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [RouterTestingModule, HttpClientModule],
-            declarations: [MainPageComponent],
-        }).compileComponents();
-    }));
+//     beforeEach(async(() => {
+//         TestBed.configureTestingModule({
+//             imports: [RouterTestingModule, HttpClientModule],
+//             declarations: [MainPageComponent],
+//         }).compileComponents();
+//     }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(MainPageComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-});
+//     beforeEach(() => {
+//         fixture = TestBed.createComponent(MainPageComponent);
+//         component = fixture.componentInstance;
+//         fixture.detectChanges();
+//     });
+// });
--- client/src/app/components/sidebar/grid-config/grid/grid.component.spec.ts
+++ client/src/app/components/sidebar/grid-config/grid/grid.component.spec.ts
@@ -1,25 +1,67 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+import { SelectionService } from '@app/services/tools/selection/selection.service';
 import { GridComponent } from './grid.component';
 
-describe('GridComponent', () => {
-  let component: GridComponent;
-  let fixture: ComponentFixture<GridComponent>;
+// tslint:disable
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GridComponent ]
-    })
-    .compileComponents();
-  }));
+fdescribe('GridComponent', () => {
+    let component: GridComponent;
+    let fixture: ComponentFixture<GridComponent>;
+    let drawingServiceSpy: jasmine.SpyObj<DrawingService>;
+    let selectionServiceSpy: jasmine.SpyObj<SelectionService>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GridComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        drawingServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas', 'setGrid']);
+        selectionServiceSpy = jasmine.createSpyObj('selectionService', ['setGridSpaces']);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        TestBed.configureTestingModule({
+            declarations: [GridComponent],
+            providers: [
+                { provide: DrawingService, useValue: drawingServiceSpy },
+                { provide: SelectionService, useValue: selectionServiceSpy },
+            ],
+            imports: [MatSliderModule, MatSlideToggleModule, FormsModule, BrowserAnimationsModule],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GridComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should call setgrid if grid is enabled', () => {
+        component.switchGridView(true);
+        expect(drawingServiceSpy.setGrid).toHaveBeenCalled();
+    });
+
+    it('should clear canvas when grid view is disabled', () => {
+        component.switchGridView(false);
+        expect(drawingServiceSpy.clearCanvas).toHaveBeenCalled();
+    });
+
+    it('should set grid spacing on grid size change if grid is enabled', () => {
+        component.isEnabled = true;
+        component.changeGridSize(10);
+        expect(selectionServiceSpy.setGridSpaces).toHaveBeenCalled();
+        expect(drawingServiceSpy.setGrid).toHaveBeenCalled();
+    });
+
+    it('should change opacity on changeOpacity call and set grid if grid is enabled', () => {
+        component.isEnabled = true;
+        component.currentOpacity = 100;
+        component.changeOpacity(50);
+        expect(drawingServiceSpy.setGrid).toHaveBeenCalled();
+        expect(component.currentOpacity).toEqual(50);
+    });
+
 });
