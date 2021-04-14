ce20947a4d3c7f053025defcd66b36551e9bc99b
Lea Kim Gilbert <lea-kim.gilbert@polymtl.ca>
2021-04-13 21:58:46 -0400

    Ajouts tests dans selection-config, 100%

--- client/src/app/components/sidebar/selection-config/selection-config.component.spec.ts
+++ client/src/app/components/sidebar/selection-config/selection-config.component.spec.ts
@@ -1,13 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MoveSelectionService } from '@app/services/selection/move-selection.service';
+import { TextService } from '@app/services/tools/text/text.service';
 import { SelectionConfigComponent } from './selection-config.component';
 
 // tslint:disable
-describe('SelectionConfigComponent', () => {
+fdescribe('SelectionConfigComponent', () => {
     let component: SelectionConfigComponent;
     let fixture: ComponentFixture<SelectionConfigComponent>;
+    let moveSelectionServiceSpy: jasmine.SpyObj<MoveSelectionService>;
+    let textServiceSpy: jasmine.SpyObj<TextService>;
 
     beforeEach(async(() => {
+        moveSelectionServiceSpy = jasmine.createSpyObj('MoveSelectionService', ['enableMagnetism']);
+        textServiceSpy = jasmine.createSpyObj('TextService', ['']);
+
         TestBed.configureTestingModule({
+            providers: [{ provide: MoveSelectionService, useValue: moveSelectionServiceSpy }],
             declarations: [SelectionConfigComponent],
         }).compileComponents();
     }));
@@ -20,5 +29,36 @@ describe('SelectionConfigComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should enable grid magnetism', () => {
+        component.enableGridMagnetism(true);
+        expect(component.isMagnetismEnabled).toBeTrue();
+        expect(moveSelectionServiceSpy.isMagnetism).toBeTrue();
+        expect(moveSelectionServiceSpy.enableMagnetism).toHaveBeenCalled();
+    });
+
+    it('should turn on magnetism feature if g is pressed for the first time', () => {
+        const input = fixture.debugElement.query(By.css('input'));
+        input.triggerEventHandler('window:keydown.m', {});
+        fixture.detectChanges();
+
+        textServiceSpy.isWriting = false;
+        component.isMagnetismEnabled = false;
+        moveSelectionServiceSpy.isMagnetism = false;
+
+        component.gIsClicked();
+        expect(component.isMagnetismEnabled).toBeTrue();
+        expect(moveSelectionServiceSpy.isMagnetism).toBeTrue();
+    });
+
+    it('should not turn on magnetism feature if isWriting of TextService is true', () => {
+        textServiceSpy.isWriting = true;
+        component.isMagnetismEnabled = false;
+        moveSelectionServiceSpy.isMagnetism = false;
+
+        component.gIsClicked();
+        expect(component.isMagnetismEnabled).toBeFalse();
+        expect(moveSelectionServiceSpy.isMagnetism).toBeFalse();
     });
 });
